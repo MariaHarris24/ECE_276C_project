4736d30617cbc8b1024d8abe720c924bcabfa5d4
Roumen Guha <roumen.guha@gmail.com>
2020-12-08 11:06:25 -0800

    Update plot_max_returns.py

--- sunrise/plot_max_returns.py
+++ sunrise/plot_max_returns.py
@@ -8,8 +8,16 @@ This is a temporary script file.
 import numpy as np
 import matplotlib.pyplot as plt
 
-rewards = np.load("./results/returns_max_walker2d.npy")
+rewards = np.load("./returns_max_walker2d.npy")
+plt.figure()
 plt.plot(rewards)
+plt.show()
+
+plt.figure()
 plt.plot(rewards[::2])
+plt.show()
+
+plt.figure()
 plt.plot(rewards[1::2])
+plt.show()
 
